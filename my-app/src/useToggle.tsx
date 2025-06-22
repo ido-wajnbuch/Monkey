9fed5b5e52a02830312ea4727cdf43a18b68e92f
ido wajnbuch <idoyosefw@gmail.com>
2025-06-22 16:02:56 +0300

    added in comment in useToggle why useCallback is important

--- my-app/src/useToggle.tsx
+++ my-app/src/useToggle.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 export const useToggle = (initialValue: boolean): [boolean, () => void] => {
     const [value, setValue] = useState(initialValue);
     const toggle = useCallback(() => { setValue(prev => !prev); }, []);
+    /*useCallback is important because it memoizes the toggle function,
+    preventing unnecessary re-renders*/ 
 
     return [value, toggle];
 }
